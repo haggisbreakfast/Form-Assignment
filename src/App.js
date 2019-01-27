b0dc0bd169ebc481d64c908742534a17629bb905
Haggis Breakfast <lj.stewart00@gmail.com>
2019-01-26 17:29:30 -0800

    adds Formik validation schema, email & password fields, & Styled Form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import styled, { createGlobalStyle } from 'styled-components';
 import { Field, FieldProps, Form, Formik, FormikProps } from 'formik';
 import { object, ref, string } from 'yup';
 
-
 const Global = createGlobalStyle`
 @import url("https://fonts.googleapis.com/css?family=Roboto+Mono");
 body {
@@ -29,6 +28,12 @@ const FormContainer = styled.div`
   height: 600px;
 `;
 
+const StyledForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+`;
+
 const HeaderContainer = styled.div`
   background: linear-gradient(#bfeaff 0%, #7cd5ff 100%);
 `;
@@ -36,6 +41,17 @@ const Header = styled.h1`
   padding: 20px 6px 20px 6px;
   font-size: 20x;
   margin: 0px;
+`;
+
+export const Error = styled.span`
+  color: grey;
+  font-size: 12px;
+  min-height: 16px;
+`;
+
+const FieldLabel = styled.label`
+  margin-bottom: 5px;
+  font-size: 16px;
 `;
 
 class App extends Component {
@@ -48,26 +64,55 @@ class App extends Component {
             <HeaderContainer>
               <Header>Create Account</Header>
             </HeaderContainer>
+
+            <Formik
+              initialValues={{
+                email: '',
+                password: '',
+              }}
+              validationSchema={object().shape({
+                email: string()
+                  .email('please enter a valid email')
+                  .required('please enter an email'),
+                password: string()
+                  .min(6)
+                  .max(20)
+                  .required('please enter a password'),
+              })}
+              render={() => (
+                <StyledForm>
+                  <Field
+                    name="email"
+                    render={({ field, form }) => (
+                      <div>
+                        <FieldLabel>email</FieldLabel>
+                        <input type="email" {...field} placeholder="" />
+                        <Error>
+                          {form.touched.email &&
+                            form.errors.email &&
+                            form.errors.email}
+                        </Error>
+                      </div>
+                    )}
+                  />
+                  <Field
+                    name="password"
+                    render={({ field, form }) => (
+                      <div>
+                        <FieldLabel>password</FieldLabel>
+                        <input type="password" {...field} placeholder="" />
+                        <Error>
+                          {form.touched.password &&
+                            form.errors.password &&
+                            form.errors.password}
+                        </Error>
+                      </div>
+                    )}
+                  />
+                </StyledForm>
+              )}
+            />
           </FormContainer>
-          <Formik
-            initialValues={{
-              email: '',
-              password: '',
-              passwordConfirm: '',
-            }}
-            validationSchema={object().shape({
-              email: string()
-                .email('please enter a valid email')
-                .required('please enter an email'),
-              password: string()
-                .min(6)
-                .max(20)
-                .required('please enter a password'),
-              passwordConfirm: string()
-                .required('please confirm password')
-                .oneOf([ref('password')], 'passwords need to match'),
-            })}
-          />
         </PageContainer>
       </div>
     );
