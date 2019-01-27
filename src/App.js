432d275421a02b6d0af7b2123d02fe4ba4abf00f
Haggis Breakfast <lj.stewart00@gmail.com>
2019-01-27 14:30:10 -0800

    adds font-awesome icon

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import styled, { createGlobalStyle } from 'styled-components';
-import { Field, FieldProps, Form, Formik, FormikProps } from 'formik';
-import { Yup, object, ref, string, number, array } from 'yup';
+import { Field, Form, Formik } from 'formik';
+import { object, string, number } from 'yup';
 import RadioButtons from './RadioButtons';
 import CheckboxButtons from './CheckboxButtons';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+// import { library, IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// library.add(faUserPlus);
 
 // local imports
 
 const Global = createGlobalStyle`
-@import url("https://fonts.googleapis.com/css?family=Roboto+Mono");
+/* @import url("https://fonts.googleapis.com/css?family=Roboto+Mono"); */
+@import url("https://fonts.googleapis.com/css?family=Kalam|Roboto+Mono");
 body {
-  font-family: 'Roboto Mono', monospace;
+  /* font-family: 'Roboto Mono', monospace; */
+  font-family: 'Kalam', cursive;
 }
 `;
 const PageContainer = styled.div`
@@ -24,7 +31,7 @@ const FormContainer = styled.div`
   margin: 20px;
   display: flex;
   flex-direction: column;
-  border: 2px solid grey;
+  border: 2px solid #cfd3d7;
   border-radius: 4px;
   box-shadow: 0px 10px 13px -7px #000000,
     1px 47px 46px -25px rgba(0, 0, 0, 0.43);
@@ -37,8 +44,13 @@ const StyledForm = styled(Form)`
   display: flex;
   flex-direction: column;
   margin: 10px;
-  /* justify-content: flex-start; */
+  justify-content: space-between;
   height: 600px;
+  border: 1px solid red;
+`;
+const Fields = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 const FieldContainer = styled.div`
@@ -46,9 +58,13 @@ const FieldContainer = styled.div`
   /* justify-content: flex-start; */
   justify-content: center;
   padding: 10px;
+  border: solid red 1px;
 `;
 
 const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+
   background: linear-gradient(#bfeaff 0%, #7cd5ff 100%);
 `;
 const Header = styled.p`
@@ -64,7 +80,7 @@ export const Error = styled.span`
 `;
 
 const FieldLabel = styled.label`
-  font-size: 16px;
+  font-size: 20px;
 `;
 
 const InputContainer = styled.div``;
@@ -72,13 +88,13 @@ const InputContainer = styled.div``;
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
-
   height: 100px;
 `;
 
 const SubmitButton = styled.button`
   background: #7cd5ff;
-  font-family: 'Roboto Mono', monospace;
+  /* font-family: 'Roboto Mono', monospace; */
+  font-family: 'Kalam', cursive;
   border-radius: 4px;
   font-size: 18px;
   &:hover {
@@ -99,6 +115,7 @@ class App extends Component {
           <FormContainer>
             <HeaderContainer>
               <Header>Create Account</Header>
+              <FontAwesomeIcon icon={faUserPlus} size="1x" color="pink" />
             </HeaderContainer>
 
             <Formik
@@ -122,60 +139,70 @@ class App extends Component {
               })}
               render={() => (
                 <StyledForm>
-                  <FieldContainer>
-                    <Field
-                      name="email"
-                      render={({ field, form }) => (
-                        <div>
-                          <FieldLabel>email</FieldLabel>
-                          <InputContainer>
-                            <input type="email" {...field} placeholder="" />
-                          </InputContainer>
-                          <Error>
-                            {form.touched.email &&
-                              form.errors.email &&
-                              form.errors.email}
-                          </Error>
-                        </div>
-                      )}
-                    />
-                  </FieldContainer>
-                  <FieldContainer>
-                    <Field
-                      name="password"
-                      render={({ field, form }) => (
-                        <div>
-                          <FieldLabel>password</FieldLabel>
-                          <InputContainer>
-                            <input type="password" {...field} placeholder="" />
-                          </InputContainer>
-                          <Error>
-                            {form.touched.password &&
-                              form.errors.password &&
-                              form.errors.password}
-                          </Error>
-                        </div>
-                      )}
-                    />
-                  </FieldContainer>
-                  <FieldContainer>
-                    <Field
-                      name="number"
-                      render={({ field, form }) => (
-                        <div>
-                          <FieldLabel>number</FieldLabel>
-                          <InputContainer>
-                            <input type="number" {...field} placeholder="" />
-                          </InputContainer>
-                          <Error>
-                            {form.touched.number &&
-                              form.errors.number &&
-                              form.errors.number}
-                          </Error>
-                        </div>
-                      )}
-                    />
-                  </FieldContainer>
+                  <Fields>
+                    <FieldContainer>
+                      <Field
+                        name="email"
+                        render={({ field, form }) => (
+                          <div>
+                            <FieldLabel>email</FieldLabel>
+                            <InputContainer>
+                              <input
+                                type="email"
+                                {...field}
+                                placeholder="lorem@ipsum.com"
+                              />
+                            </InputContainer>
+                            <Error>
+                              {form.touched.email &&
+                                form.errors.email &&
+                                form.errors.email}
+                            </Error>
+                          </div>
+                        )}
+                      />
+                    </FieldContainer>
+                    <FieldContainer>
+                      <Field
+                        name="password"
+                        render={({ field, form }) => (
+                          <div>
+                            <FieldLabel>password</FieldLabel>
+                            <InputContainer>
+                              <input
+                                type="password"
+                                {...field}
+                                placeholder=""
+                              />
+                            </InputContainer>
+                            <Error>
+                              {form.touched.password &&
+                                form.errors.password &&
+                                form.errors.password}
+                            </Error>
+                          </div>
+                        )}
+                      />
+                    </FieldContainer>
+                    <FieldContainer>
+                      <Field
+                        name="number"
+                        render={({ field, form }) => (
+                          <div>
+                            <FieldLabel>number</FieldLabel>
+                            <InputContainer>
+                              <input type="number" {...field} placeholder="" />
+                            </InputContainer>
+                            <Error>
+                              {form.touched.number &&
+                                form.errors.number &&
+                                form.errors.number}
+                            </Error>
+                          </div>
+                        )}
+                      />
+                    </FieldContainer>
+                  </Fields>
                   <RadioButtons />
                   <CheckboxButtons />
                   <ButtonContainer>
--- src/RadioButtons.js
+++ src/RadioButtons.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 class RadioButtons extends React.Component {
   select(event) {
